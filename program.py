38ed2c3c0eba24521fe0d02f3233544ce03c6eb5
Kavinraja.D <119875375+d-kavinraja@users.noreply.github.com>
2023-10-21 09:16:43 +0530

    Create program.py

---
+++ program.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from sklearn import linear_model
+df = pd.read_csv("carsemission.csv")
+X = df[['Weight', 'Volume']]
+y = df['CO2']
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+print('Coefficients:', regr.coef_)
+print('Intercept:',regr.intercept_)
+predictedCO2 = regr.predict([[3300, 1300]])
+print('Predicted CO2 for the corresponding weight and volume',predictedCO2)
